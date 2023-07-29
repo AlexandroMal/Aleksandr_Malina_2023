08846b86c56900cbc3b509d604f5d2bef2fd5d4e
AlexandroMal <Sashamalina8695@gmail.com>
2023-07-29 17:42:53 +0300

    Added Task_3

---
+++ Lesson_1/Task_3/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Linq.Expressions;
+
+List<int> numbers = new List<int>();
+
+while (numbers.Count < 10)
+{
+    Console.Write($"Enter number {numbers.Count + 1}: ");
+
+    try
+    {
+        numbers.Add(Convert.ToInt32(Console.ReadLine()));
+    }
+
+    catch
+    {
+        Console.WriteLine("Incorrect value, try again!");
+    }
+}
+
+while (true)
+{
+    Console.Write("\nEnter number for create dublicate:");
+
+    try
+    {
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        var indexValue = numbers.IndexOf(number);
+
+        if (indexValue != -1)
+        {
+            numbers.Insert(indexValue + 1, number);
+
+            // Output list
+            Console.Write("Your list with dublicated: ");
+            foreach (int i in numbers)
+            {
+                Console.Write($"{i},");
+            }
+        }
+
+        else
+        {
+            Console.WriteLine("No such number is found in the list. Try again!");
+        }
+    }
+
+    catch
+    {
+        Console.WriteLine("Incorect valuy, try again!");
+    }
+
+}
